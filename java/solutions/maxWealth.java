7ec677d261576aa60c83113b258e625082fac180
Josue Herrera <54568984+josh-hrr@users.noreply.github.com>
2024-01-05 21:42:22 -0600

    Create maxWealth.java

---
+++ java/solutions/maxWealth.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public int maximumWealth(int[][] array) { 
+        ArrayList<Integer> myList = new ArrayList<>(); 
+        
+        int maxValueFromSums = 0;  
+        for(int i = 0; i<array.length; i++){  
+            int sum = 0; 
+            for(int j = 0; j<array[i].length; j++){ 
+                sum = sum + array[i][j];
+                System.out.println(array[i][j]); 
+            } 
+            myList.add(sum);
+            System.out.println("sum is: " + sum);
+            System.out.println();
+        }  
+        for(int i = 0; i<myList.size(); i++){ 
+            System.out.println("sums found: " + myList.get(i));
+        }  
+            maxValueFromSums = Collections.max(myList); 
+            System.out.println();
+            System.out.println("max wealth is: " + maxValueFromSums);
+
+            return maxValueFromSums;
+    }
+}
