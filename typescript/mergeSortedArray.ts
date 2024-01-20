0ffd436307fba5b49d9f1022e9b10d7e3b56e403
Josue Herrera <54568984+josh-hrr@users.noreply.github.com>
2024-01-19 22:34:19 -0600

    Update mergeSortedArray.ts

--- typescript/mergeSortedArray.ts
+++ typescript/mergeSortedArray.ts
@@ -1,8 +1,8 @@
+
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    nums1.splice(m, n, ...nums2);
+    nums1.splice(m, nums1.length - 1, ...nums2);
     nums1.sort((a, b) => a - b); 
 };
-
 /*
 testData:
 
