6fa57008c4d5f38dbc13fb3793c5c1a443dab7c9
Josue Herrera <54568984+josh-hrr@users.noreply.github.com>
2024-02-01 22:47:14 -0600

    Create bestTimeToBuySellStock.ts

---
+++ typescript/bestTimeToBuySellStock.ts
@@ -0,0 +1,18 @@
+function maxProfit(prices: number[]): number { 
+    if (!prices || prices.length < 2) {
+        return 0;
+    }
+
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+    }
+
+    return maxProfit;
+};
